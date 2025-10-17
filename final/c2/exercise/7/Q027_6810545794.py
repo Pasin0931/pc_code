4cb64933749639187dd79472d60186208f9d77b9
Pasin <pasin0931@gmail.com>
2025-10-17 09:51:01 +0700

    idk

---
+++ final/c2/exercise/7/Q027_6810545794.py
@@ -0,0 +1,31 @@
+# Name: Pasin Makcharoen # Student ID: 6810545794
+
+import csv
+from pathlib import Path
+
+path_ = Path.cwd() / "inventory.csv"
+fp = open(path_, mode="r", encoding="utf-8")
+reader = csv.reader(fp)
+
+all_headers = next(reader)
+
+id_ = []
+name_ = []
+cata = []
+stock = []
+    
+for each_ in reader:
+    # print(each_)
+    id_.append(each_[0])
+    name_.append(each_[1])
+    cata.append(each_[2])
+    stock.append(each_[3])
+
+in_ = input("Enter category to filter by: ")
+if in_ not in cata:
+    print(f"No items found in category \'{in_}\'.")
+else:
+    print(f"Items in category \'{in_}\': ")
+    for i in range(len(id_)):
+        if cata[i] == in_:
+            print(f"Item: {name_[i]}, Stock: {stock[i]}")
